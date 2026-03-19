81bb1a2e823b0ec4ac25edf15f183950960de622
uebahide <uebahide@gmail.com>
2026-03-19 16:47:39 +0100

    create useRealtimeMessages hooks

---
+++ hooks/useRealtimeMessages.ts
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect } from "react";
+import { createClient } from "@/lib/supabase/client";
+import { Message } from "@/types/api/message";
+import { RealtimeChannel } from "@supabase/supabase-js";
+
+export function useRealtimeMessages(
+  roomId: string | null,
+  onInsert: (newMessage: Message) => void,
+) {
+  useEffect(() => {
+    if (!roomId) return;
+
+    const supabase = createClient();
+    let channel: RealtimeChannel | null = null;
+    let cancelled = false;
+
+    const setup = async () => {
+      await supabase.realtime.setAuth();
+
+      if (cancelled) return;
+
+      channel = supabase
+        .channel(`channel:${roomId}`, {
+          config: { private: true },
+        })
+        .on("broadcast", { event: "INSERT" }, (payload: any) => {
+          console.log("broadcast payload", payload);
+          onInsert(payload.payload.record as Message);
+        })
+        .subscribe((status) => {
+          console.log("Realtime status:", status);
+        });
+    };
+
+    setup();
+
+    return () => {
+      cancelled = true;
+      if (channel) {
+        supabase.removeChannel(channel);
+      }
+    };
+  }, [roomId]);
+}
