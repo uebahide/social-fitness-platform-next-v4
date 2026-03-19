9dfc8dbb0a25c8b213472ddf0d88b8f96307b866
uebahide <uebahide@gmail.com>
2026-03-19 18:38:32 +0100

    change main padding

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-h-screen w-full overflow-y-auto bg-gray-100 p-8">
+    <div className="max-h-screen w-full overflow-y-auto bg-gray-100 p-6">
       {children}
     </div>
   );
