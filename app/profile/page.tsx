f8694da64839341b5bbee764496c8072cf7336de
uebahide <uebahide@gmail.com>
2026-03-24 09:49:49 +0100

    update profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,10 +5,14 @@ import { getTotalActivityCount } from "@/lib/server/getTotalActivityCount";
 import { Input } from "@/components/form/Input";
 import { FormRow } from "@/components/form/FormRow";
 import { SubmitButton } from "@/components/buttons/SubmitButton";
+import { getCurrentUser } from "@/lib/server/getCurrentUser";
+import { PersonalInfoCard } from "./PersonalInfoCard";
 
 export default async function Profile() {
   const latestActivity = await getLatestActivity();
   const activityCount = await getTotalActivityCount();
+  const user = await getCurrentUser();
+
   return (
     <div className="grid grid-cols-[1fr_2fr] gap-4 grid-rows-[3fr_2fr]">
       <UserProfileCard
@@ -17,67 +21,17 @@ export default async function Profile() {
         showMyActivitiesLink={false}
         className="col-span-1"
       />
-      <Card className="col-span-1 row-span-2">
-        <form className="space-y-8 p-4">
-          <div className="space-y-4">
-            <header className="text-lg font-bold">Profile information</header>
-            <div className="grid grid-cols-2 gap-4 pl-3">
-              <FormRow>
-                <label htmlFor="name">First Name</label>
-                <Input id="first_name" name="first_name" />
-              </FormRow>
-              <FormRow>
-                <label htmlFor="name">Last Name</label>
-                <Input id="last_name" name="last_name" />
-              </FormRow>
-              <FormRow>
-                <label htmlFor="name">Display Name</label>
-                <Input id="display_name" name="display_name" />
-              </FormRow>
-              <FormRow>
-                <label htmlFor="name">Gender</label>
-                <Input id="gender" name="gender" />
-              </FormRow>
-              <FormRow>
-                <label htmlFor="name">Nationality</label>
-                <Input id="nationality" name="nationality" />
-              </FormRow>
-            </div>
-          </div>
-          <div className="space-y-4">
-            <header className="text-lg font-bold">Contact Info</header>
-            <div className="grid grid-cols-2 gap-4 pl-3">
-              <FormRow>
-                <label htmlFor="name">Email</label>
-                <Input id="first_name" name="first_name" />
-              </FormRow>
-              <FormRow>
-                <label htmlFor="name">Website</label>
-                <Input id="display_name" name="display_name" />
-              </FormRow>
-            </div>
-          </div>
-          <div className="space-y-4">
-            <header className="text-lg font-bold">About the User</header>
-            <div className="pl-3">
-              <FormRow>
-                <label htmlFor="name">Biographical Info</label>
-                <textarea
-                  id="about"
-                  name="about"
-                  className="w-full border border-gray-200 rounded-sm p-2 h-30"
-                />
-              </FormRow>
-            </div>
-          </div>
-        </form>
-      </Card>
+      <PersonalInfoCard user={user} />
 
       <Card>
         <form className="space-y-4 p-4">
           <FormRow>
             <label htmlFor="name">Old Password</label>
-            <Input id="old_password" name="old_password" />
+            <Input
+              id="old_password"
+              name="old_password"
+              defaultValue={user?.password}
+            />
           </FormRow>
           <FormRow>
             <label htmlFor="name">New Password</label>
